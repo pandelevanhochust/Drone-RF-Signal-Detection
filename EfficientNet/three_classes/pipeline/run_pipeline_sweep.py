2ff320cdc99a11a9674b89b0eb72ffe2f6242608
pandelevanhochust <toan.lv225160@sis.hust.edu.vn>
2026-06-29 18:31:59 +0700

    fix sh

--- EfficientNet/three_classes/pipeline/run_pipeline_sweep.py
+++ EfficientNet/three_classes/pipeline/run_pipeline_sweep.py
@@ -172,7 +172,7 @@ def get_args():
     p.add_argument("--model",        default="../new_three_classes.tflite")
     p.add_argument("--labels",       default="class_names.txt",
                    help="One class per line: DRONE, DRONE_SIGNAL, NO_DRONE")
-    p.add_argument("--addr",         default="192.168.5.115")
+    p.add_argument("--addr",         default="192.168.5.111")
     p.add_argument("--gain",         type=float, default=35.0)
     p.add_argument("--cpu",          action="store_true",
                    help="Disable NPU delegate, run on CPU only")
